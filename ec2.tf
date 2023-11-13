7d5dd64c7c4b3bc2856d23a2df3c7d8b177696d6
Nik Webb <nik.webb@ons.gov.uk>
2023-11-13 09:31:21 +0000

    updating messaes

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami           = data.aws_ami.ubuntu-22-04.id
-  instance_type = "t2.medium"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "example-instance"
